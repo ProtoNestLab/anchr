f6e3c713d4dda93f0ead52158c330d8558568baa
vlinglandy <1747024205@qq.com>
2026-04-16 20:33:18 +0800

    feat: 开发环境引用源码

--- apps/demo/vite.config.ts
+++ apps/demo/vite.config.ts
@@ -1,6 +1,14 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import { resolve } from 'path'
 
 export default defineConfig({
   plugins: [vue()],
+  resolve: {
+    alias: {
+      '@anchor-sdk/core': resolve(__dirname, '../../packages/core/src'),
+      '@anchor-sdk/vue': resolve(__dirname, '../../packages/vue/src'),
+      '@anchor-sdk/ui': resolve(__dirname, '../../packages/ui/src'),
+    },
+  },
 })
